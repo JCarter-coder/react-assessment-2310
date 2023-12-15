0a3f93c1e67f16aa4d6dc04e28b0b9af606adc7c
Jesse_Carter <jessetcarter@hotmail.com>
2023-12-15 09:25:18 -0700

    "complete"

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,10 +1,22 @@
-const Products = () => {
+const Products = ({ products }) => {
     // Instructions:
     //     1. Import the products state as props
+    // Done
     //     2. Loop over useing #map 
+    // Done
     //     3. Show the product name in an h1, and the product price in a p tag
+    // Done
 
-    return null
+    return (
+        <div>
+            {products.productData.map((product) => (
+              <div>
+                <h1>{product.name}</h1>
+                <p>{product.pricing}</p>
+              </div>
+            ))}
+        </div>
+    )
 }
 
 export default Products
